6f99e12672c3a7a855091c03c43734f352e67346
William <149340282+maylew@users.noreply.github.com>
2024-06-28 12:03:21 -0700

    Update UserGroup.java
    added creation time field along with getter and setter

--- src/UserGroup.java
+++ src/UserGroup.java
@@ -5,13 +5,21 @@ public class UserGroup {
     private String groupId;
     private Set<User> users;
     private Set<UserGroup> subGroups;
+    private Long creationTime;
     public UserGroup(String groupId) {
         this.groupId = groupId;
         this.users = new HashSet<>();
         this.subGroups = new HashSet<>();
+        this.creationTime = 0L;
     }
     public String getGroupId() {
         return groupId;
+    }
+    public Long getCreationTime() {
+        return creationTime;
+    }
+    public void setCreationTime(Long timeCreated) {
+        creationTime = timeCreated;
     }
     public void addUser(User user) {
         users.add(user);
